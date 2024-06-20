8423d13b5ae834f7508349df1c4f7524e897bd0c
Utkarsh Mishra <nasautkarsh@gmail.com>
2024-06-21 00:23:27 +0530

    Added required Models/Schema
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

---
+++ Server/models/Course.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    instructor:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    whatYouWillLearn:{
+        type:String,
+    },
+    courseContent:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Section",
+    }],
+    ratingAndReviews:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "RatingAndReview",
+    }],
+    price:{
+        type:Number,
+    },
+    thumbnail:{
+        type:String,
+    },
+    tag:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tag",
+    },
+    studentsEnrolled : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required:true,
+    }],
+});
+
+module.exports = mongoose.model("Course", courseSchema);
