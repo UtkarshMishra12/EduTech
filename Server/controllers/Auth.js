dbe74a4b96da450f6d375fb8241c3f77ed633e39
Utkarsh Mishra <nasautkarsh@gmail.com>
2024-06-22 22:01:11 +0530

    SignUp Controller logic
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- Server/controllers/Auth.js
+++ Server/controllers/Auth.js
@@ -65,7 +65,110 @@ exports.sendOTP = async (req,res) =>{
 
 
 //Signup
+exports.signUp = async (req,res) =>{
+    try{
+        //fetch data
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            accountType,
+            contactNumber,
+            otp
+        }= req.body;
 
+        //validation 
+        if(!firstName || !lastName || !email || !password || !confirmPassword || !otp){
+            res.status(403).json({
+                success:false,
+                message: "Please fill the form correctly",
+            })
+        }
+
+        //check if both the password are same or not
+        if(password !== confirmPassword){
+            res.status(500).json({
+                success:true,
+                message: "Password does not match, please write correctly",
+            })
+        }
+
+        //check if user already present
+        const existingUser= await User.findOne({email});
+        if(existingUser){
+             res.status(400).json({
+                 success:false,
+                 message:"User already exist",
+            })
+        }
+
+        //find most recent OTP stored for the user
+        const recentOtp = await User.findOne({email}).sort({createdAt:-1}).limit(1);
+        console.log(recentOtp);
+
+        //validateOTP
+        if(recentOtp.length == 0){
+            res.status(400).json({
+                success:false,
+                message:"OTP Not found",
+            })
+        }
+        else if(otp !== recentOtp.otp){
+            //Invalid OTP
+            res.status(400).json({
+                success:false,
+                message:"Invalid OTP",
+            })
+        }
+
+
+        //Hash Password
+        let hashedPassword;
+        try{
+            hashedPassword = await bcrypt.hash(password, 10);
+        }
+        catch(error){
+            res.status(404).json({
+                success:false,
+                message: "Error in hashing the password",
+            })
+        }
+
+        const profileDetails = await Profile.create({
+            gender:null,
+            dateOfBirth:null,
+            about:null,
+            contactNumber:null,
+        })
+
+        //create entry in DB
+        const data = await User.create({
+            firstName,
+            lastName,
+            password:hashedPassword,
+            email,
+            accountType,
+            contactNumber,
+            additionalDetails: profileDetails._id,
+            image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
+        });
+
+        res.status(200).json({
+            success:true,
+            message: "User registered successfully",
+            user,
+        })
+
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:"Error in signingUp",
+        })
+    }
+}
 
 
 //Login
