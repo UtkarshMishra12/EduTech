481cccf0a4d2251d1afde5833c6ed8d1eb1eac34
Utkarsh Mishra <nasautkarsh@gmail.com>
2024-07-01 23:37:28 +0530

    deleteProfile Controller (API)
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- Server/controllers/Profile.js
+++ Server/controllers/Profile.js
@@ -33,10 +33,44 @@ exports.updateProfile = async (req,res) =>{
     }
     catch(error){
         return res.status(400).json({
-            success:true,
+            success:false,
             message:"Error in updating Profile",
         })
     }
 };
 
 //delete account
+exports.deleteAccount = async (req,res) =>{
+    try{
+        //get id
+        const id = req.user.id;
+        //validate id
+        const userDetails = await User.findById(id);
+
+        if(!userDetails){
+            return res.status(500).json({
+                success:false,
+                message:"User does not exist",
+            })
+        };
+
+        const profileId = userDetails.additionalDetails;
+        //delete profile
+        await Profile.findByIdAndDelete(profileId); // (_id:userDetails.additionalDetails)
+        //TODO : UnEnroll user from all the enrolled courses
+        //delete user
+        await User.findByIdAndDelete({_id:id});
+        //return response
+        return res.status(200).json({
+            success:true,
+            message: "User Deleted successfully",
+        });
+        //TODO: Schedule deletion of account
+    }
+    catch(error){
+        return res.status(400).json({
+            success:false,
+            message:"Error in deleting Profile",
+        })
+    }
+}
