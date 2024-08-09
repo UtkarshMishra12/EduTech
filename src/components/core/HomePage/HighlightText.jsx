bbe51dbb8886343d3f0320889c854e546e532c5a
Utkarsh Mishra <nasautkarsh@gmail.com>
2024-08-10 00:02:20 +0530

    Section-1 Video and Code/Heading
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- src/components/core/HomePage/HighlightText.jsx
+++ src/components/core/HomePage/HighlightText.jsx
@@ -5,6 +5,7 @@ function HighlightText ({text}) {
         <span className="bg-gradient-to-b from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] text-transparent bg-clip-text font-bold">
             {" "}
             {text}
+            {" "}
         </span>
     )
 }
