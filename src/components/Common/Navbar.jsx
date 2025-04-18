10717c4ba1454978754f74806f76e1654a7ef544
Utkarsh Mishra <nasautkarsh@gmail.com>
2025-04-18 23:14:01 +0200

    Navbar Implementation along with Slices
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -3,8 +3,17 @@ import { Link, Route, matchPath } from "react-router-dom";
 import logo from "../../assets/Logo/Logo-Full-Light.png";
 import {NavbarLinks} from "../../data/navbar-links";
 import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import ProfileDropDown from "../core/Auth/ProfileDropDown";
 
 function Navbar(){
+
+    const {token} = useSelector((state) => state.auth);
+    const {user} = useSelector((state) => state.profile);  
+    const {totalItems} = useSelector((state) => state.cart);
+    
+    
     const location = useLocation();
     const matchRoute = (route) =>{
         return matchPath({path:route}, location.pathname);
@@ -24,7 +33,11 @@ function Navbar(){
                             <li key={index}>
                                 {
                                     link.title === "Catalog" ? 
-                                    (<div></div>) 
+                                    (
+                                        <div>
+
+                                        </div>
+                                    ) 
                                     : 
                                     (
                                         <Link to={link?.path}>
@@ -46,6 +59,50 @@ function Navbar(){
 
                 {/* Login and Sign Up Button and Dashoard*/}
                 <div className="flex items-center  ">
+                    {
+                        user && user?.accountType !== "Instructor" && (
+                            <Link to="/dashboard/cart" className="relative text-white">
+                                <AiOutlineShoppingCart/>
+                                {
+                                    <div className="absolute top-[-8px] right-[-4px] flex items-center justify-center h-4 w-4 rounded-full bg-yellow-100">
+                                        {
+                                            totalItems > 0 && (
+                                              <span>
+                                                {totalItems}
+                                           </span>
+                                            )
+                                        }
+                                    </div>
+                                }
+                            </Link>
+                        )
+                    }
+
+                    {
+                        token===null &&(
+                            <Link to="/login">
+                              <button className="border border-richblack-700 bg-richblue-800 px-[12px] py-[8px] text-richblack-100 rounded-md mr-3">
+                                 Login
+                              </button>
+                            </Link>
+                        )
+                    }
+
+                    {
+                        token===null &&(
+                            <Link to="/signup" >
+                              <button className="border border-richblack-700 bg-richblue-800 px-[12px] py-[8px] text-richblack-100 rounded-md">
+                                 Signup
+                            </button>
+                            </Link>
+                        )
+                    }
+
+                    {
+                        token !== null && (
+                            <ProfileDropDown/>
+                        )
+                    }
 
                 </div>
 
