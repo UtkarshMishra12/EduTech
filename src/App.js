42fb1ff4ea81a7874f9db5c3a07b2d5bcf11461a
Utkarsh Mishra <nasautkarsh@gmail.com>
2025-04-19 14:55:39 +0200

    Added Error and ForgotPassword Page
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup"
 import Navbar from "./components/Common/Navbar";
+import ForgotPassword from "./pages/ForgotPassword";
+import OpenRoute from "./components/core/Auth/OpenRoute";
 
 function App() {
   return (
@@ -11,8 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<OpenRoute><Login/></OpenRoute>} />
+        <Route path="/signup" element={<OpenRoute><Signup/></OpenRoute>} />
+        <Route path="/forgot-password" element={<OpenRoute><ForgotPassword/></OpenRoute>} />
       </Routes>
     </div>
   );
