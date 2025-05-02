1c0efb0dae8ff69202b652c367c13327d7375cab
Utkarsh Mishra <nasautkarsh@gmail.com>
2025-05-02 13:40:02 +0200

    Enrolled Courses Page and profileAPI service
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import MyProfile from "./components/core/Dashboard/MyProfile";
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./components/core/Auth/PrivateRoute";
 import Settings from "./components/core/Dashboard/Settings";
+import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses"
 
 function App() {
   return (
@@ -39,6 +40,7 @@ function App() {
         >
         <Route path="dashboard/my-profile" element={<MyProfile />} />
         <Route path="dashboard/settings" element={<Settings />} />
+        <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
         </Route>
       </Routes>
     </div>
