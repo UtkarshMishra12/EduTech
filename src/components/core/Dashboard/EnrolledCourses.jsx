a2f7a5af25d98e9b1f689aa7d7b783c97bc23fc9
Utkarsh Mishra <nasautkarsh@gmail.com>
2025-05-02 12:57:31 +0200

    Profile API and Enrolled Courses Page inital setup
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

---
+++ src/components/core/Dashboard/EnrolledCourses.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function EnrolledCourses() {
+
+    return(
+        <div>
+
+            <div>Enrolled Courses</div>
+        </div>
+    )
+}
+
+export default EnrolledCourses;
